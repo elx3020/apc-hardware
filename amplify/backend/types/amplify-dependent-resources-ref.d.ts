68dd58b91105b1cb46787679df9b51962b526f3e
elx3020 <estebannl28@gmail.com>
2022-05-21 15:47:52 +0200

    amplify change, cognito, bucket, functions, graphql api and changes in the index.html

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,40 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "apchardwared39ae4c1": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "ProductsImages": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    },
+    "function": {
+        "processPayment": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        },
+        "createOrder": {
+            "Name": "string",
+            "Arn": "string",
+            "Region": "string",
+            "LambdaExecutionRole": "string"
+        }
+    },
+    "api": {
+        "apchardware": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ ReactDOM.render(
       <App />
     </Authenticator.Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById("apc-root")
 );
 
 // If you want to start measuring performance in your app, pass a function
